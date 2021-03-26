6272901a1b7004c57217161c4c864b4a617bee2b
Katherine Gott <katherinegott@Katherines-MacBook-Pro.local>
2021-03-26 10:37:22 -0500

    table driven tests

--- messages_test.go
+++ messages_test.go
@@ -1,6 +1,8 @@
 package messages
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestGreet(t *testing.T) {
 	got := Greet("Gopher")
@@ -11,6 +13,23 @@ func TestGreet(t *testing.T) {
 	}
 }
 
+func TestGreetTableDriven(t *testing.T) {
+	scenarios := []struct {
+		input  string
+		expect string
+	}{
+		{input: "Gopher", expect: "Hello, Gopher\n"},
+		{input: "", expect: "Hello, \n"},
+	}
+	for _, s := range scenarios {
+		got := Greet(s.input)
+		if got != s.expect {
+			t.Errorf("Did not get expected result for input %v. Wanted %q, got: %q\n", s.input, got, s.expect)
+		}
+	}
+
+}
+
 func TestDepart(t *testing.T) {
 	got := depart("Gopher")
 	expect := "Goodbye, Gopher\n"
@@ -18,10 +37,4 @@ func TestDepart(t *testing.T) {
 	if got != expect {
 		t.Errorf("Did not get expected result. Wanted %q, got: %q\n", expect, got)
 	}
-}
-
-func TestFailureTypes(t *testing.T) {
-	t.Error("Error signals a failed test, but doesn't stop the rest of the test from executing")
-	t.Fatal("Fatal will fail the test and stop its execution")
-	t.Error("This will never print since it is preceeded by an immediate failure")
 }
